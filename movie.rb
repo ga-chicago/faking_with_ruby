04defa0b1963a7560b1759a2862ec0fc0a42a801
James Traver <jtraverjr@gmail.com>
2016-03-16 12:13:02 -0500

    woosh

---
+++ movie.rb
@@ -0,0 +1,8 @@
+class Movie
+  #attr_accessor
+  # 1. have a constructor that accepts a URL for OMDB
+  # 2. The constructor should then use HTTParty.get to get the data\
+  # 3. You will then SET instance variables for @title @director @plot
+  # 4. Implement a to_s, to_hash, to_json
+  # 5. self.method_name == this.methodName() in JS
+end
